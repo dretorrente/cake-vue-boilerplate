0f42aa8b86aa5a2686ed63c1caf289eef55e1dbb
davetorrente <dave.torrente@gmail.com>
2018-08-10 17:03:24 +0800

    initial commit

---
+++ webroot/js/vuejs/src/Animes.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+  <li v-for="anime in animes">
+  {{anime}}
+  </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      animes: ['One piece','Naruto','Bleach']
+    }
+  }
+}
+</script>
